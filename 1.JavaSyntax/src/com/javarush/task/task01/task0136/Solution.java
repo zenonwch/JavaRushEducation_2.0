2923cbea3cac84736d0dfc340c1f90ec291a80f0
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-02-20 21:29:21 +0200

    JavaSyntax, level02, lesson08, task0136 - done

--- 1.JavaSyntax/src/com/javarush/task/task01/task0136/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0136/Solution.java
@@ -5,11 +5,11 @@ package com.javarush.task.task01.task0136;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println(getWeight(888));
     }
 
-    public static double getWeight(int weightEarth) {
-        //напишите тут ваш код
+    public static double getWeight(final int weightEarth) {
+        return 0.17 * weightEarth;
     }
 }
