bb46949a0e9a8d4ab92be001fa04c7cfa1d1ff0a
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-02-20 20:38:34 +0200

    JavaSyntax, level01, lesson03, task0109 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+    public static void main(final String[] args) {
+        final int a = 1;
+        final int b = 2;
+    }
+}
