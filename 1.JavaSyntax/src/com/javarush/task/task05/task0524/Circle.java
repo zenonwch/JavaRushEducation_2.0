7a20c7e836b2772dd64545113cfea8cb635e0229
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-02-24 21:26:53 +0200

    JavaSyntax, level05, lesson09, task0524 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0524/Circle.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task05.task0524;
+
+/* 
+Основа колеса
+*/
+
+public class Circle {
+    private double x;
+    private double y;
+    private double r;
+
+    public Circle(final double x, final double y, final double r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public static void main(final String[] args) {
+
+    }
+}
