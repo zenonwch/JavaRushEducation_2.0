129176617be8be3ebf7d0351ddf97fec0d2ac2a3
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-02-25 23:27:30 +0200

    JavaSyntax, level06, lesson08, task0612 - done

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0612/Calculator.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task06.task0612;
+
+/* 
+Калькулятор
+*/
+
+public class Calculator {
+    public static int plus(final int a, final int b) {
+        return a + b;
+    }
+
+    public static int minus(final int a, final int b) {
+        return a - b;
+    }
+
+    public static int multiply(final int a, final int b) {
+        return a * b;
+    }
+
+    public static double division(final int a, final int b) {
+        return (double) a / b;
+    }
+
+    public static double percent(final int a, final int b) {
+        return (double) a * b / 100;
+    }
+
+    public static void main(final String[] args) {
+
+    }
+}
