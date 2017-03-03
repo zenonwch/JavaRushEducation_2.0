34832fa5dbdddadec507cab3fd3051cf47237232
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-03 22:57:08 +0200

    JavaMultithreading, level04, lesson14, task3608 - subtask04 done

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -1,15 +1,15 @@
 package com.javarush.task.task36.task3608;
 
 import com.javarush.task.task36.task3608.controller.Controller;
-import com.javarush.task.task36.task3608.model.FakeModel;
+import com.javarush.task.task36.task3608.model.MainModel;
 import com.javarush.task.task36.task3608.model.Model;
 import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Solution {
     public static void main(final String[] args) {
-        Model model = new FakeModel();
-        UsersView usersView = new UsersView();
-        Controller controller = new Controller();
+        final Model model = new MainModel();
+        final UsersView usersView = new UsersView();
+        final Controller controller = new Controller();
 
         usersView.setController(controller);
         controller.setModel(model);
