895606a5c50974f73d3a2ddc3a08f66987f34fe0
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-06 19:55:58 +0200

    JavaMultithreading, level08, lesson08, task2811 - done

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2811/ReadWriteMap.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2811/ReadWriteMap.java
@@ -8,26 +8,28 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class ReadWriteMap<K, V> {
     private final Map<K, V> map;
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
-    private final Lock readLock;
-    private final Lock writeLock;
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
 
-    public ReadWriteMap(Map<K, V> map) {
+    public ReadWriteMap(final Map<K, V> map) {
         this.map = map;
     }
 
-    public V put(K key, V value) {
+    public V put(final K key, final V value) {
+        writeLock.lock();
         try {
             return map.put(key, value);
         } finally {
-
+            writeLock.unlock();
         }
     }
 
-    public V get(K key) {
+    public V get(final K key) {
+        readLock.lock();
         try {
             return map.get(key);
         } finally {
-
+            readLock.unlock();
         }
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2811/Solution.java
@@ -4,8 +4,10 @@ package com.javarush.task.task28.task2811;
 ReentrantReadWriteLock
 */
 
+import java.util.LinkedHashMap;
+
 public class Solution {
-    public static void main(String[] args) {
-        ReadWriteMap<Integer, String> linkedSafeMap = new ReadWriteMap<>(new LinkedHashMap<>());
+    public static void main(final String[] args) {
+        final ReadWriteMap<Integer, String> linkedSafeMap = new ReadWriteMap<>(new LinkedHashMap<>());
     }
 }
