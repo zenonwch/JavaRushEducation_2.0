0b7674380a7031a2fa69977fbc9e72cf44c9cb93
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-08 20:18:57 +0200

    JavaMultithreading, level09, lesson09, task2913 - done

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -10,22 +10,24 @@ public class Solution {
     private static int numberA;
     private static int numberB;
 
-    public static String recursion(int a, int b) {
-        if (a > b) {
-            return a + " " + recursion(a - 1, b);
-        } else {
-            if (a == b) {
-                return Integer.toString(a);
-            }
-            return a + " " + recursion(a + 1, b);
+    public static String getAllNumbersBetween(final int a, final int b) {
+        final int direction = a < b ? 1 : -1;
+        int nextStep = a;
+        final StringBuilder sb = new StringBuilder("");
+
+        while (nextStep != b) {
+            sb.append(nextStep).append(' ');
+            nextStep += direction;
         }
+
+        return sb.append(nextStep).toString();
     }
 
-    public static void main(String[] args) {
-        Random random = new Random();
+    public static void main(final String[] args) {
+        final Random random = new Random();
         numberA = random.nextInt() % 1_000;
         numberB = random.nextInt() % 10_000;
-        System.out.println(recursion(numberA, numberB));
-        System.out.println(recursion(numberB, numberA));
+        System.out.println(getAllNumbersBetween(numberA, numberB));
+        System.out.println(getAllNumbersBetween(numberB, numberA));
     }
 }
