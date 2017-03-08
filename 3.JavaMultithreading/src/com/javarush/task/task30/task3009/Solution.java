dafe8094b733e22f0df79ed7fc6a2fb47a50ed93
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-08 21:06:29 +0200

    JavaMultithreading, level10, lesson02, task3009 - done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3009/Solution.java
@@ -8,6 +8,20 @@ import java.util.Set;
 */
 
 public class Solution {
+    private static Set<Integer> getRadix(final String number) {
+        final Set<Integer> result = new HashSet<>();
+        try {
+            for (int i = 2; i <= 36; i++) {
+                final String newNumber = Integer.toString(Integer.parseInt(number), i);
+                final String reversNumber = new StringBuilder(newNumber).reverse().toString();
+                if (newNumber.equals(reversNumber))
+                    result.add(i);
+            }
+        } catch (final NumberFormatException ignored) {
+        }
+        return result;
+    }
+
     public static void main(final String[] args) {
         System.out.println(getRadix("112"));        //expected output: [3, 27, 13, 15]
         System.out.println(getRadix("123"));        //expected output: [6]
