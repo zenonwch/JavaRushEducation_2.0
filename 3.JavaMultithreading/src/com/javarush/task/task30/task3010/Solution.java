190dd7a0941a25de307134b6c04f09d7ae21fb24
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-09 21:31:47 +0200

    JavaMultithreading, level10, lesson02, task3010 - done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3010/Solution.java
@@ -4,8 +4,27 @@ package com.javarush.task.task30.task3010;
 Минимальное допустимое основание системы счисления
 */
 
+import java.math.BigInteger;
+import java.util.regex.Pattern;
+
 public class Solution {
-    public static void main(String[] args) {
-        //напишите тут ваш код
+    private static final Pattern COMPILE = Pattern.compile("^[a-zA-Z0-9]*$");
+
+    public static void main(final String[] args) {
+        try {
+            if (!COMPILE.matcher(args[0]).matches()) {
+                System.out.println("incorrect");
+            } else {
+                for (int i = 2; i <= 36; i++) {
+                    try {
+                        new BigInteger(args[0], i);
+                        System.out.println(i);
+                        break;
+                    } catch (final NumberFormatException ignore) {
+                    }
+                }
+            }
+        } catch (final Exception ignore) {
+        }
     }
 }
