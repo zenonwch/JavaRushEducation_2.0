d6c90aa81a3cf116c5b5e94a3f8d9008a9e2616c
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-11 17:47:01 +0200

    JavaMultithreading, level10, lesson06, task3013 - done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3013/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3013/Solution.java
@@ -5,7 +5,7 @@ package com.javarush.task.task30.task3013;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Solution solution = new Solution();
         int number = Integer.MAX_VALUE - 133;
         System.out.println(Integer.toString(number, 2));
@@ -15,6 +15,11 @@ public class Solution {
     }
 
     public int resetLowerBits(int number) {
-        //напишите тут ваш код
+        number |= number >> 1;
+        number |= number >> 2;
+        number |= number >> 4;
+        number |= number >> 8;
+        number |= number >> 16;
+        return number & ~(number >> 1);
     }
 }
