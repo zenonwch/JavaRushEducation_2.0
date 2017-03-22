f1af0a6d87261197bf2e273c83bd43ecfc3aa702
Andrey Veshtard <andrey.veshtard@office.ctco.lv>
2017-03-23 00:13:25 +0200

    JavaCollections, level01, lesson04, task3112 - done

--- 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task31.task3112;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,15 +12,26 @@ import java.nio.file.Paths;
 */
 public class Solution {
 
-    public static void main(String[] args) throws IOException {
-        Path passwords = downloadFile("https://www.amigo.com/ship/secretPassword.txt", Paths.get("D:/MyDownloads"));
+    public static void main(final String[] args) throws IOException {
+        final Path passwords = downloadFile("https://www.amigo.com/ship/secretPassword.txt", Paths.get("C:/MyDownloads"));
 
-        for (String line : Files.readAllLines(passwords)) {
+        for (final String line : Files.readAllLines(passwords)) {
             System.out.println(line);
         }
     }
 
-    public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
-        // implement this method
+    public static Path downloadFile(final String urlString, final Path downloadDirectory) throws IOException {
+
+        final URL url = new URL(urlString);
+        final InputStream is = url.openStream();
+        final Path tmpFile = Files.createTempFile("tmp-", "");
+        Files.copy(is, tmpFile);
+        is.close();
+
+        final String fileName = urlString.substring(urlString.lastIndexOf('/') + 1);
+        final Path dwnldPath = Paths.get(downloadDirectory + "/" + fileName);
+        Files.move(tmpFile, dwnldPath);
+
+        return dwnldPath;
     }
 }
