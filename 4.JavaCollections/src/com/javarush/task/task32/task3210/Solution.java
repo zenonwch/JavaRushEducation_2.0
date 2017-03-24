c537094f51f39784f935006df0d3589b6f675c26
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-24 21:43:38 +0200

    JavaCollections, level02, lesson02, task3210 - done

--- 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3210/Solution.java
@@ -8,7 +8,29 @@ import java.io.RandomAccessFile;
 */
 
 public class Solution {
-    public static void main(String... args) {
+    public static void main(final String... args) {
+        final String fileName = args[0];
+        final String strNumber = args[1];
+        final String text = args[2];
 
+        try (final RandomAccessFile raf = new RandomAccessFile(fileName, "rw")) {
+            final int number = Integer.parseInt(strNumber);
+            raf.seek(number);
+
+            final int textSize = text.length();
+            final byte[] buffer = new byte[textSize];
+            raf.read(buffer, 0, textSize);
+
+            final long fileLength = raf.length();
+            raf.seek(fileLength);
+
+            final String textFromFile = convertByteToString(buffer);
+            raf.write(textFromFile.equals(text) ? "true".getBytes() : "false".getBytes());
+        } catch (final IOException ignored) {
+        }
+    }
+
+    private static String convertByteToString(final byte[] readBytes) {
+        return new String(readBytes);
     }
 }
