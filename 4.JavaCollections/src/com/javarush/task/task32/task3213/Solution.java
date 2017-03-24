1f662d5c24455f439d0cff55d8a5bae31e86969f
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-24 23:33:45 +0200

    JavaCollections, level02, lesson04, task3213 - correct solution

--- 4.JavaCollections/src/com/javarush/task/task32/task3213/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3213/Solution.java
@@ -14,8 +14,11 @@ public class Solution {
         final StringReader reader2 = new StringReader("A a");
         System.out.println(decode(reader2, -3));  //Hello Amigo
         final StringReader reader3 = new StringReader("Z z");
-        System.out.println(decode(reader3, 5));  //Hello Amigo
-
+        System.out.println(decode(reader3, 3));  //Hello Amigo
+        final StringReader reader4 = new StringReader("Khoor Dpljr");
+        System.out.println(decode(reader4, -29));  //Hello Amigo
+        final StringReader reader5 = new StringReader("Khoor Dpljr");
+        System.out.println(decode(reader5, 49));  //Hello Amigo
     }
 
     public static String decode(final StringReader reader, final int key) throws IOException {
@@ -25,7 +28,16 @@ public class Solution {
         final StringBuilder sb = new StringBuilder("");
         int c;
         while ((c = reader.read()) != -1) {
-            c += key;
+            if (c > 64 && c < 91) {
+                c += key % 26;
+                if (c < 65) c += 26;
+                if (c > 90) c -= 26;
+            }
+            if (c > 96 && c < 123) {
+                c += key % 26;
+                if (c < 97) c += 26;
+                if (c > 122) c -= 26;
+            }
             sb.append((char) c);
         }
 
