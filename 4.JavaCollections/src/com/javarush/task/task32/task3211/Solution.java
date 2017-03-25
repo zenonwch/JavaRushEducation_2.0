2562425e877e3176c5eb70a416cee3e9e276f096
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-25 17:01:11 +0200

    JavaCollections, level02, lesson06, task3211 - done

--- 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
@@ -9,17 +9,26 @@ import java.security.MessageDigest;
 */
 
 public class Solution {
-    public static void main(String... args) throws Exception {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
+    public static void main(final String... args) throws Exception {
+        final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        final ObjectOutputStream oos = new ObjectOutputStream(bos);
         oos.writeObject(new String("test string"));
         oos.flush();
         System.out.println(compareMD5(bos, "5a47d12a2e3f9fecf2d9ba1fd98152eb")); //true
 
     }
 
-    public static boolean compareMD5(ByteArrayOutputStream byteArrayOutputStream, String md5) throws Exception {
+    public static boolean compareMD5(final ByteArrayOutputStream byteArrayOutputStream, final String md5) throws Exception {
+        final MessageDigest md = MessageDigest.getInstance("MD5");
+        final byte[] baos = byteArrayOutputStream.toByteArray();
+        final byte[] hash = md.digest(baos);
 
-        return false;
+        final StringBuilder sb = new StringBuilder(2 * hash.length);
+        for (final byte b : hash) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+        final String baosMd5 = sb.toString();
+
+        return baosMd5.equals(md5);
     }
 }
