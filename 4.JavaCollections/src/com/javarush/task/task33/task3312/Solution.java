46bc759da44b31370d5adbce27e03687fecd7c19
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-26 16:48:54 +0300

    JavaCollections, level03, lesson05, task3312 - done

--- 4.JavaCollections/src/com/javarush/task/task33/task3312/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3312/Solution.java
@@ -7,14 +7,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 Сериализация зоопарка
 */
 public class Solution {
-    public static void main(String[] args) throws JsonProcessingException {
-        Zoo.Dog dog = new Zoo.Dog("doggy");
-        Zoo.Cat cat = new Zoo.Cat("catty");
-        Zoo zoo = new Zoo();
+    public static void main(final String[] args) throws JsonProcessingException {
+        final Zoo.Dog dog = new Zoo.Dog("doggy");
+        final Zoo.Cat cat = new Zoo.Cat("catty");
+        final Zoo zoo = new Zoo();
         zoo.animals.add(dog);
         zoo.animals.add(cat);
 
-        String result = new ObjectMapper().writeValueAsString(zoo);
+        final String result = new ObjectMapper().writeValueAsString(zoo);
 
         System.out.println(result);
     }
--- 4.JavaCollections/src/com/javarush/task/task33/task3312/Zoo.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3312/Zoo.java
@@ -10,28 +10,40 @@ import java.util.List;
 public class Zoo {
     public List<Animal> animals = new ArrayList<>();
 
+    @JsonTypeInfo(
+            use = JsonTypeInfo.Id.NAME,
+            include = JsonTypeInfo.As.PROPERTY,
+            property = "type"
+    )
+
+    @JsonSubTypes({
+            @JsonSubTypes.Type(value = Dog.class, name = "dog"),
+            @JsonSubTypes.Type(value = Cat.class, name = "cat")
+    })
     public static class Animal {
-        public Animal(String name) {
+        public Animal(final String name) {
             this.name = name;
         }
 
         public String name;
     }
 
+    @JsonTypeName("dog")
     public static class Dog extends Animal {
 
         public double barkVolume;
 
-        public Dog(String name) {
+        public Dog(final String name) {
             super(name);
         }
     }
-    
+
+    @JsonTypeName("cat")
     public static class Cat extends Animal {
         boolean likesCream;
         public int lives;
 
-        public Cat(String name) {
+        public Cat(final String name) {
             super(name);
         }
     }
