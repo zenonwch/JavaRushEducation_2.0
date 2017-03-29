ad6c61a23f61b56919ec04693a625a163c28d49e
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-29 21:49:20 +0300

    JavaCollections, level04, lesson02, task3402 - done

--- 4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
@@ -4,15 +4,16 @@ package com.javarush.task.task34.task3402;
 Факториал с помощью рекурсии
 */
 public class Solution {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
+    public static void main(final String[] args) {
+        final Solution solution = new Solution();
 
         System.out.println(solution.factorial(9));     //362880
         System.out.println(solution.factorial(0));     //1
         System.out.println(solution.factorial(1));     //1
     }
 
-    public int factorial(int n) {
-        return 0;
+    public int factorial(final int n) {
+        if (n == 0 || n == 1) return 1;
+        return factorial(n -1) * n;
     }
 }
