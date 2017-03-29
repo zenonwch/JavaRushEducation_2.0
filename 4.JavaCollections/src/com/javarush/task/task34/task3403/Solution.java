d0b8f06db8830ebf6a7865400cb64c9f50819059
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-29 22:29:52 +0300

    JavaCollections, level04, lesson02, task3403 - done

--- 4.JavaCollections/src/com/javarush/task/task34/task3403/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3403/Solution.java
@@ -4,6 +4,18 @@ package com.javarush.task.task34.task3403;
 Разложение на множители с помощью рекурсии
 */
 public class Solution {
-    public void recursion(int n) {
+
+    public void recursion(final int n) {
+        final int l = (int) Math.sqrt(n);
+        for (int i = 2; i <= l; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                recursion(n / i);
+                break;
+            }
+            if (i == l) {
+                System.out.print(n);
+            }
+        }
     }
 }
