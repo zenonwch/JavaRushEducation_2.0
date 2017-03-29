cf9eb5631701bd6f150eea21b2f385a29f028f86
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-03-29 21:32:14 +0300

    JavaCollections, level04, lesson02, task3401 - done

--- 4.JavaCollections/src/com/javarush/task/task34/task3401/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3401/Solution.java
@@ -4,8 +4,8 @@ package com.javarush.task.task34.task3401;
 Числа Фибоначчи с помощью рекурсии
 */
 public class Solution {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
+    public static void main(final String[] args) {
+        final Solution solution = new Solution();
 
         System.out.println(solution.fibonacci(9));     //34
         System.out.println(solution.fibonacci(5));     //5
@@ -13,7 +13,9 @@ public class Solution {
         System.out.println(solution.fibonacci(1));     //1
     }
 
-    public int fibonacci(int n) {
-        return 0;
+    public int fibonacci(final int n) {
+        if (n <= 0) return 0;
+        if (n <= 2) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
