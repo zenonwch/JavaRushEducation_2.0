d6f247b5ad76ac915611ae85badd37f116cf3e69
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-09 16:12:24 +0300

    JavaCollections, level05, lesson04, task3502 - done

--- 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
@@ -1,13 +1,17 @@
 package com.javarush.task.task35.task3502;
 
-/* 
+import com.javarush.task.task35.task3502.Solution.SomeClass;
+
+import java.util.List;
+
+/*
 Знакомство с дженериками
 */
-public class Solution {
-    public static class SomeClass {
+public class Solution <T extends List<SomeClass>> {
+    public static class SomeClass<T extends Number> {
     }
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
     }
 }
