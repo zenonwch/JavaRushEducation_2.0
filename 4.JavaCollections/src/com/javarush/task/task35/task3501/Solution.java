acc260aba2146ff4b83c86bb11b948882e2d9098
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-09 12:02:59 +0300

    JavaCollections, level05, lesson04, task3501 - done

--- 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3501/Solution.java
@@ -3,7 +3,7 @@ package com.javarush.task.task35.task3501;
 Вызов статического метода
 */
 public class Solution {
-    public static void main(String[] args) {
-        Number number = GenericStatic.<Number>someStaticMethod(new Integer(3));
+    public static void main(final String[] args) {
+        final Number number = GenericStatic.someStaticMethod(new Integer(3));
     }
 }
