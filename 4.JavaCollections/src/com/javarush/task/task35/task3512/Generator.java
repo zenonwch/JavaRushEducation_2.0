9db6d3970e172f7b2e50e6c0da175e6fce456840
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-11 23:29:08 +0300

    JavaCollections, level05, lesson08, task3512 - done

--- 4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
@@ -1,7 +1,16 @@
 package com.javarush.task.task35.task3512;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Generator<T> {
-    T newInstance() {
-        return new T();
+
+    private Class<T> clazz;
+
+    public Generator(final Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    T newInstance() throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        return clazz.getConstructor().newInstance();
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task35/task3512/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3512/Solution.java
@@ -1,12 +1,14 @@
 package com.javarush.task.task35.task3512;
 
-/* 
+import java.lang.reflect.InvocationTargetException;
+
+/*
 Генератор объектов
 */
 public class Solution {
 
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-        Generator<Event> eventGenerator = new Generator<>(Event.class);
+    public static void main(final String[] args) throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        final Generator<Event> eventGenerator = new Generator<>(Event.class);
 
         System.out.println(eventGenerator.newInstance().getId());
         System.out.println(eventGenerator.newInstance().getId());
