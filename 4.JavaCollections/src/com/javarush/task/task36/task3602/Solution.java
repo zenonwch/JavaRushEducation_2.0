167bb18b0b76cce3430256bede706f03ef741e6d
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-14 22:46:10 +0300

    JavaCollections, level06, lesson06, task3602 - done

--- 4.JavaCollections/src/com/javarush/task/task36/task3602/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3602/Solution.java
@@ -1,16 +1,50 @@
 package com.javarush.task.task36.task3602;
 
-import java.util.Collections;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 /* 
 Найти класс по описанию
 */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println(getExpectedClass());
     }
 
-    public static Class getExpectedClass() {
+    public static Class<?> getExpectedClass() {
+
+        for (final Class<?> cls : Collections.class.getDeclaredClasses()) {
+
+            final Set<Class<?>> interfaces = new HashSet<>();
+            getAllInterfaces(cls, interfaces);
+
+            if (interfaces.contains(List.class)
+                    && Modifier.isPrivate(cls.getModifiers())
+                    && Modifier.isStatic(cls.getModifiers())) {
+                try {
+                    final Constructor<?> constructor = cls.getDeclaredConstructor();
+                    constructor.setAccessible(true);
+                    final List<?> list = (List<?>) constructor.newInstance();
+                    list.get(0);
+                } catch (final IndexOutOfBoundsException e) {
+                    return cls;
+                } catch (final Exception ignored) {
+                }
+            }
+        }
+
         return null;
+    }
+
+    private static void getAllInterfaces(final Class<?> clazz, final Set<Class<?>> interfaces) {
+        if (clazz == null) return;
+        if (clazz.getInterfaces().length == 0) return;
+        interfaces.addAll(Arrays.asList(clazz.getInterfaces()));
+        for (final Class<?> intf : clazz.getInterfaces()) {
+            getAllInterfaces(intf, interfaces);
+            getAllInterfaces(intf.getSuperclass(), interfaces);
+        }
+        getAllInterfaces(clazz.getSuperclass(), interfaces);
     }
 }
