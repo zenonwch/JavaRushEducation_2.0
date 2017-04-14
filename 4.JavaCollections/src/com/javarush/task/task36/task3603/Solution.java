565a7b03c9921e7ede16f9f4657d4221cce5639e
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-14 22:56:50 +0300

    JavaCollections, level06, lesson06, task3603 - done

--- 4.JavaCollections/src/com/javarush/task/task36/task3603/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3603/Solution.java
@@ -8,17 +8,17 @@ import java.util.concurrent.CopyOnWriteArrayList;
 Поиск класса по описанию
 */
 public class Solution {
-    public static void main(String... args) {    //it's correct line
-        ClassNameToBeReplaced<String> list = new ClassNameToBeReplaced();
+    public static void main(final String... args) {    //it's correct line
+        final CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
         list.add("A");
         list.add("B");
         list.add("C");
         list.remove("B");
-        List<String> collection = Arrays.asList(new String[]{"B", "C", "D", "B"});
+        final List<String> collection = Arrays.asList("B", "C", "D", "B");
 
-        list.methodNameToBeReplaced(collection);
+        list.addAllAbsent(collection);
 
-        for (String string : list) {
+        for (final String string : list) {
             System.out.println(string);
         }
         /* Expected output
