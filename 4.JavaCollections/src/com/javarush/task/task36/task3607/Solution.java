17093af80961bd50017d00000499ab87d9608b32
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-18 01:36:01 +0300

    JavaCollections, level06, lesson10, task3607 - done

--- 4.JavaCollections/src/com/javarush/task/task36/task3607/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3607/Solution.java
@@ -2,10 +2,8 @@ package com.javarush.task.task36.task3607;
 
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -19,43 +17,36 @@ public class Solution {
     }
 
     public static Class getExpectedClass() {
-        final Set<String> classNames = new HashSet<>();
+        final Set<Class> classes = new HashSet<>();
+        final String packageName = Queue.class.getPackage().getName().replaceAll("\\.", "/");
 
-        //to avoid this mess you can start search within java.util package
-        final URLClassLoader classLoader = (URLClassLoader) Thread.currentThread().getContextClassLoader();
-        final URL[] jarsPath = classLoader.getURLs();
+        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        final URL enn = cl.getResource(packageName + "/Queue.class");
+        final String jarFilePath = enn.toString().split("file:")[1].split("!/" + packageName)[0];
 
-        for (final URL jarPath : jarsPath) {
-            if (jarPath.toString().endsWith("rt.jar")) {
-                try {
-                    final JarFile jarFile = new JarFile(jarPath.getPath().toString());
-                    final Enumeration<JarEntry> en = jarFile.entries();
-                    while (en.hasMoreElements()) {
-                        final String fileName = en.nextElement().getName();
-                        if (fileName.startsWith("java/util") && fileName.endsWith(".class")) {
-                            classNames.add(fileName);
-                        }
-                    }
-                } catch (final IOException ignored) {
+        try {
+            final JarFile jarFile = new JarFile(jarFilePath);
+            final List<? extends JarEntry> jarEntryList = Collections.list(jarFile.entries());
+
+            for (final JarEntry jarEntry : jarEntryList) {
+                if (jarEntry.isDirectory()) continue;
+                final String fileName = jarEntry.getName();
+                if (fileName.startsWith(packageName + "/concurrent") && fileName.endsWith(".class") && !fileName.contains("$")) {
+                    final String classPath = fileName.substring(0, fileName.length() - 6).replaceAll("[\\|/]", ".");
+                    classes.add(Class.forName(classPath));
                 }
             }
+        } catch (final IOException | ClassNotFoundException ignored) {
         }
 
-        for (final String className : classNames) {
-            try {
-                final String fileName = className.substring(0, className.length() - 6).replace('/', '.');
-                final Class cls = Class.forName(fileName);
-                if (Queue.class.isAssignableFrom(cls) && cls.getEnclosingClass() == null) {
-                    final Field[] fields = cls.getDeclaredFields();
-                    final Method[] methods = cls.getDeclaredMethods();
-                    if (Arrays.stream(methods)
-                            .filter(m -> m.getName().contains("peekExpired"))
-                            .flatMap(m -> Arrays.stream(fields))
-                            .anyMatch(f -> f.getType().equals(Thread.class))) {
+        for (final Class cls : classes) {
+            if (Queue.class.isAssignableFrom(cls) && cls.getEnclosingClass() == null && !cls.isInterface()) {
+                final Method[] methods = cls.getDeclaredMethods();
+                for (final Method m : methods) {
+                    if (m.getName().toLowerCase().contains("expired")) {
                         return cls;
                     }
                 }
-            } catch (final ClassNotFoundException ignored) {
             }
         }
         return null;
