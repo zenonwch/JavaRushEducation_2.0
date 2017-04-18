207facfe79fbfb4b0bd2aef796e31b938b80254d
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-18 20:46:22 +0300

    JavaCollections, level06, lesson10, task3610 - done

--- 4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3610/MyMultiMap.java
@@ -8,58 +8,78 @@ public class MyMultiMap<K, V> extends HashMap<K, V> implements Cloneable, Serial
     private HashMap<K, List<V>> map;
     private int repeatCount;
 
-    public MyMultiMap(int repeatCount) {
+    public MyMultiMap(final int repeatCount) {
         this.repeatCount = repeatCount;
         map = new HashMap<>();
     }
 
     @Override
     public int size() {
-        //напишите тут ваш код
+        return values().size();
     }
 
     @Override
-    public V put(K key, V value) {
-        //напишите тут ваш код
+    public V put(final K key, final V value) {
+        List<V> list = new LinkedList<>();
+        V prevLastValue = null;
+        if (map.get(key) != null) {
+            list = map.get(key);
+            prevLastValue = list.get(list.size() - 1);
+            if (list.size() == repeatCount) {
+                list.remove(0);
+            }
+        }
+        list.add(value);
+        map.put(key, list);
+        return prevLastValue;
     }
 
     @Override
-    public V remove(Object key) {
-        //напишите тут ваш код
+    public V remove(final Object key) {
+        V value = null;
+        if (map.get(key) == null || map.get(key).isEmpty()) {
+            map.remove(key);
+        } else if (map.get(key).size() == 1) {
+            value = map.get(key).get(0);
+            map.remove(key);
+        } else {
+            value = map.get(key).remove(0);
+        }
+        return value;
     }
 
     @Override
     public Set<K> keySet() {
-        //напишите тут ваш код
+        return map.keySet();
     }
 
     @Override
     public Collection<V> values() {
-        //напишите тут ваш код
+        return map.values().stream().collect(ArrayList::new, List::addAll, List::addAll);
     }
 
     @Override
-    public boolean containsKey(Object key) {
-        //напишите тут ваш код
+    public boolean containsKey(final Object key) {
+        return map.containsKey(key);
     }
 
     @Override
-    public boolean containsValue(Object value) {
-        //напишите тут ваш код
+    public boolean containsValue(final Object value) {
+        return values().contains(value);
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("{");
-        for (Map.Entry<K, List<V>> entry : map.entrySet()) {
+        final StringBuilder sb = new StringBuilder("{");
+        for (final Map.Entry<K, List<V>> entry : map.entrySet()) {
             sb.append(entry.getKey());
-            sb.append("=");
-            for (V v : entry.getValue()) {
+            sb.append('=');
+            for (final V v : entry.getValue()) {
                 sb.append(v);
                 sb.append(", ");
             }
         }
-        String substring = sb.substring(0, sb.length() - 2);
-        return substring + "}";
+        final String substring = sb.substring(0, sb.length() - 2);
+        return substring + '}';
     }
 }
