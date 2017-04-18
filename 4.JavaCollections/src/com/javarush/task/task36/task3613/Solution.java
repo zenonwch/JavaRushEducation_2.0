ee223b6b1d83c3be99d4feedcc74059ee7f6b82f
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-18 21:53:20 +0300

    JavaCollections, level06, lesson10, task3613 - done

--- 4.JavaCollections/src/com/javarush/task/task36/task3613/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3613/Solution.java
@@ -1,15 +1,17 @@
 package com.javarush.task.task36.task3613;
 
 
-/* 
+import java.util.concurrent.SynchronousQueue;
+
+/*
 Найти класс по описанию
 */
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println(getExpectedClass());
     }
 
     public static Class getExpectedClass() {
-        return null;
+        return SynchronousQueue.class;
     }
 }
