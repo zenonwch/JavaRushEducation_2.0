5708889f64492c8ffd40157dcdb584fb3322ecd4
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-19 22:43:02 +0300

    JavaCollections, level06, lesson15, task3310 - subtask03 done

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,11 +1,24 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 public class Shortener {
-    public Long getId(final String string) {
-        return null;
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(final StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(final Long id) {
-        return null;
+    public synchronized Long getId(final String string) {
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        }
+        storageStrategy.put(lastId++, string);
+        return lastId;
+    }
+
+    public synchronized String getString(final Long id) {
+        return storageStrategy.getValue(id);
     }
 }
