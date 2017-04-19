1215afb5f1e4e3f0463ff4762d69bc881f41c48f
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-19 23:53:30 +0300

    JavaCollections, level06, lesson15, task3310 - subtask05 done

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/HashMapStorageStrategy.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task33.task3310.strategy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HashMapStorageStrategy implements StorageStrategy {
+    private HashMap<Long, String> data;
+
+    @Override
+    public boolean containsKey(final Long key) {
+        return data.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(final String value) {
+        return data.containsValue(value);
+    }
+
+    @Override
+    public void put(final Long key, final String value) {
+        data.put(key, value);
+    }
+
+    @Override
+    public Long getKey(final String value) {
+        return data.entrySet().stream()
+                .filter(e -> Objects.equals(e.getValue(), value))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public String getValue(final Long key) {
+        return data.get(key);
+    }
+}
