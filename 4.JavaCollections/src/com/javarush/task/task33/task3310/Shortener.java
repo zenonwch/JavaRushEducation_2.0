8b38215c2e5b48b869573c58622af9a6b7156a5d
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-19 22:26:48 +0300

    JavaCollections, level06, lesson15, task3310 - subtask01 done

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task33.task3310;
+
+public class Shortener {
+    public Long getId(final String string) {
+        return null;
+    }
+
+    public String getString(final Long id) {
+        return null;
+    }
+}
