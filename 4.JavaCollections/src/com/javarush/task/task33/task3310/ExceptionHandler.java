d5b2c529dc141d966a0625d462788900c13b79f9
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-19 22:53:09 +0300

    JavaCollections, level06, lesson15, task3310 - subtask04 done

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/ExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task33.task3310;
+
+public class ExceptionHandler {
+    public static void log(final Exception e) {
+        System.out.println(e.getMessage());
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Helper.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task33.task3310;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class Helper {
+    public static String generateRandomString() {
+        final SecureRandom random = new SecureRandom();
+
+        return new BigInteger(130, random).toString(32);
+    }
+
+    public static void printMessage(final String message) {
+        System.out.println(message);
+    }
+}
