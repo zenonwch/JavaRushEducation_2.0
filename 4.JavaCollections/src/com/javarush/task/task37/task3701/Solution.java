f89caeb644c2b211a19824719933c5cd3a925c71
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-23 11:08:04 +0300

    JavaCollections, level07, lesson04, task3701 - done

--- 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
@@ -1,19 +1,21 @@
 package com.javarush.task.task37.task3701;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /* 
 Круговой итератор
 */
 public class Solution<T> extends ArrayList<T> {
-    public static void main(String[] args) {
-        Solution<Integer> list = new Solution<>();
+    public static void main(final String[] args) {
+        final Solution<Integer> list = new Solution<>();
         list.add(1);
         list.add(2);
         list.add(3);
 
         int count = 0;
-        for (Integer i : list) {
+        for (final Integer i : list) {
             //1 2 3 1 2 3 1 2 3 1
             System.out.print(i + " ");
             count++;
@@ -23,6 +25,32 @@ public class Solution<T> extends ArrayList<T> {
         }
     }
 
-    public class RoundIterator {
+    public class RoundIterator implements Iterator<T>{
+        Iterator<T> iterator = Solution.super.iterator();
+
+        @Override
+        public boolean hasNext() {
+            return size() != 0;
+        }
+
+        @Override
+        public T next() {
+            try {
+                return iterator.next();
+            } catch (final NoSuchElementException ignored) {
+                iterator = Solution.super.iterator();
+                return iterator.next();
+            }
+        }
+
+        @Override
+        public void remove() {
+            iterator.remove();
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new RoundIterator();
     }
 }
