f6565e0ac7831edbba2f1ace54beeead8a5c79b3
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-04-25 21:15:02 +0300

    JavaCollections, level07, lesson08, task3712 - done

--- 4.JavaCollections/src/com/javarush/task/task37/task3712/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3712/Solution.java
@@ -1,18 +1,19 @@
 package com.javarush.task.task37.task3712;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /* 
 Шаблонный метод
 */
 public class Solution {
-    public static void main(String[] args) {
-        List<Game> games = new ArrayList<>();
+    public static void main(final String[] args) {
+        final List<Game> games = new ArrayList<>();
         games.add(new Football());
         games.add(new Basketball());
         games.add(new Tennis());
 
-        for(Game game : games) {
+        for(final Game game : games) {
             game.run();
             System.out.println("---------------------------------------------");
         }
