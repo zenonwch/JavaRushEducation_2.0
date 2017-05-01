f90c6570cfb39551348ec20fd685b27ee78f9c1d
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-01 20:21:44 +0300

    JavaCollections, level08, lesson04, task3803 - done

--- 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
@@ -6,14 +6,16 @@ Runtime исключения (unchecked exception)
 
 public class VeryComplexClass {
     public void methodThrowsClassCastException() {
-
+        final Integer i = 10;
+        final Object o = i;
+        System.out.println((String) o);
     }
 
     public void methodThrowsNullPointerException() {
-
+        final String s = null;
+        System.out.println(s.length());
     }
 
-    public static void main(String[] args) {
-
+    public static void main(final String[] args) {
     }
 }
