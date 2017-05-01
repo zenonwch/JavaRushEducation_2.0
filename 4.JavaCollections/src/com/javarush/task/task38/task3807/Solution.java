2b2057da16a9207f616d5f4c49b2d2bd4ddc1edf
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-01 22:28:24 +0300

    JavaCollections, level08, lesson08, task3807 - done

--- 4.JavaCollections/src/com/javarush/task/task38/task3807/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3807/Solution.java
@@ -3,24 +3,31 @@ package com.javarush.task.task38.task3807;
 /* 
 Предопределенные типы аннотаций (Predefined Annotation Types)
 */
-
+@Deprecated
 public class Solution {
-
+    @Deprecated
     private String[] arguments;
 
-    public Solution(String... arguments) {
+    @Deprecated
+    @SafeVarargs
+    public Solution(final String... arguments) {
         this.arguments = arguments;
     }
 
+    @Deprecated
     public void voidMethod() throws Exception {
     }
 
-    public static void main(String[] args) throws Exception {
+    @Deprecated
+    public static void main(final String[] args) throws Exception {
         new Solution().new SubSolution().voidMethod();
     }
 
+    @Deprecated
     class SubSolution extends Solution {
 
+        @Override
+        @Deprecated
         public void voidMethod() throws Exception {
             super.voidMethod();
         }
