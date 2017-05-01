190870404ea169273a56b844e8c2c3fa65d889fb
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-01 20:13:36 +0300

    JavaCollections, level08, lesson04, task3802 - done

--- 4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
@@ -4,12 +4,16 @@ package com.javarush.task.task38.task3802;
 Проверяемые исключения (checked exception)
 */
 
+import java.text.SimpleDateFormat;
+
 public class VeryComplexClass {
     public void veryComplexMethod() throws Exception {
-        //напишите тут ваш код
+        final String string = "12abc";
+        System.out.println(new SimpleDateFormat("dd.MM.yyyy").parse(string));
     }
 
-    public static void main(String[] args) {
-
+    public static void main(final String[] args) throws Exception {
+        VeryComplexClass vcc = new VeryComplexClass();
+        vcc.veryComplexMethod();
     }
 }
