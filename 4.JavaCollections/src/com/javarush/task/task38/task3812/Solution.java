4385fc9ad39cd38feb54548681aee600200ef7b3
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-02 19:37:57 +0300

    JavaCollections, level08, lesson10, task3812 - done

--- 4.JavaCollections/src/com/javarush/task/task38/task3812/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3812/Solution.java
@@ -5,7 +5,7 @@ package com.javarush.task.task38.task3812;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         printFullyQualifiedNames(Solution.class);
         printFullyQualifiedNames(SomeTest.class);
 
@@ -13,11 +13,25 @@ public class Solution {
         printValues(SomeTest.class);
     }
 
-    public static boolean printFullyQualifiedNames(Class c) {
-        return true;
+    public static boolean printFullyQualifiedNames(final Class c) {
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            final PrepareMyTest annotation = (PrepareMyTest) c.getAnnotation(PrepareMyTest.class);
+            for (final String name : annotation.fullyQualifiedNames()) {
+                System.out.println(name);
+            }
+            return true;
+        }
+        return false;
     }
 
-    public static boolean printValues(Class c) {
-        return true;
+    public static boolean printValues(final Class c) {
+        if (c.isAnnotationPresent(PrepareMyTest.class)) {
+            final PrepareMyTest annotation = (PrepareMyTest) c.getAnnotation(PrepareMyTest.class);
+            for (final Class cls : annotation.value()) {
+                System.out.println(cls.getSimpleName());
+            }
+            return true;
+        }
+        return false;
     }
 }
