975a2701f52b61e0de6189251b00eb63dfed287f
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-03 08:04:13 +0300

    JavaCollections, level09, lesson09, task3902 - done

--- 4.JavaCollections/src/com/javarush/task/task39/task3902/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3902/Solution.java
@@ -8,18 +8,18 @@ import java.io.InputStreamReader;
 Биты были биты
 */
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(final String[] args) throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("Please type in a number: ");
 
-        long l = Long.parseLong(reader.readLine());
-        String result = isWeightEven(l) ? "even" : "odd";
+        final long l = Long.parseLong(reader.readLine());
+        final String result = isWeightEven(l) ? "even" : "odd";
         System.out.println("Number of ones in a given number is " + result);
 
     }
 
-    public static boolean isWeightEven(long number) {
-        return false;
+    public static boolean isWeightEven(final long number) {
+        return Long.bitCount(number) %2 == 0;
     }
 }
