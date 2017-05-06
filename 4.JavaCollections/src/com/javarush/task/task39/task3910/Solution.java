c5ef2fdde44935f2045ef7e5ffe9c05dc21be62d
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-06 21:11:09 +0300

    JavaCollections, level09, lesson09, task3910 - done

--- 4.JavaCollections/src/com/javarush/task/task39/task3910/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3910/Solution.java
@@ -4,14 +4,19 @@ package com.javarush.task.task39.task3910;
 isPowerOfThree
 */
 public class Solution {
-    public static void main(String[] args) {
-
+    public static void main(final String[] args) {
+        System.out.println(isPowerOfThree(9));
+        System.out.println(isPowerOfThree(12));
+        System.out.println(isPowerOfThree(0));
+        System.out.println(isPowerOfThree(-9));
     }
 
     public static boolean isPowerOfThree(int n) {
-        if (n % 3 == 0) {
-            return true;
+        if (n == 0) return false;
+
+        while (n % 3 == 0) {
+            n /= 3;
         }
-        return false;
+        return n == 1;
     }
 }
