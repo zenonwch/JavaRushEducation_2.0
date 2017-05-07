59beb25f57e14cf4aaa2cd8997aca02cc5a304a7
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-07 10:56:22 +0300

    JavaCollections, level10, lesson04, task4001 - done

--- 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
@@ -1,8 +1,6 @@
 package com.javarush.task.task40.task4001;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -11,23 +9,30 @@ POST, а не GET
 */
 
 public class Solution {
-    public static void main(String[] args) throws Exception {
-        Solution solution = new Solution();
-        solution.sendPost(new URL("http://requestb.in/1cse9qt1"), "name=zapp&mood=good&locale=&id=777");
+    public static void main(final String[] args) throws Exception {
+        final Solution solution = new Solution();
+        solution.sendPost(new URL("http://requestb.in/172q3ke1"), "name=zapp&mood=good&locale=&id=777");
     }
 
-    public void sendPost(URL url, String urlParameters) throws Exception {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    public void sendPost(final URL url, final String urlParameters) throws Exception {
+        final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
         connection.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-        int responseCode = connection.getResponseCode();
+        final OutputStream outputStream = connection.getOutputStream();
+        final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+        bufferedWriter.write(urlParameters);
+        bufferedWriter.flush();
+        bufferedWriter.close();
+
+        final int responseCode = connection.getResponseCode();
         System.out.println("Response Code: " + responseCode);
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String responseLine;
-        StringBuilder response = new StringBuilder();
+        final StringBuilder response = new StringBuilder();
 
         while ((responseLine = bufferedReader.readLine()) != null) {
             response.append(responseLine);
