80dcd1c2e055cd7dafe8c78cca064ba16e5a9be2
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-07 20:13:17 +0300

    JavaCollections, level10, lesson04, task4011 - done

--- 4.JavaCollections/src/com/javarush/task/task40/task4011/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4011/Solution.java
@@ -9,12 +9,27 @@ import java.net.URL;
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(final String[] args) throws IOException {
         decodeURLString("https://www.amrood.com/index.htm?language=en#j2se");
+        decodeURLString("www.google.com");
     }
 
-    public static void decodeURLString(String s) throws MalformedURLException {
+    public static void decodeURLString(final String s) throws MalformedURLException {
+        try {
+            final URL url = new URL(s);
 
+            System.out.println(url.getProtocol());
+            System.out.println(url.getAuthority());
+            System.out.println(url.getFile());
+            System.out.println(url.getHost());
+            System.out.println(url.getPath());
+            System.out.println(url.getPort());
+            System.out.println(url.getDefaultPort());
+            System.out.println(url.getQuery());
+            System.out.println(url.getRef());
+        } catch (MalformedURLException ignored) {
+            System.out.println("Parameter " + s + " is not a valid URL.");
+        }
     }
 }
 
