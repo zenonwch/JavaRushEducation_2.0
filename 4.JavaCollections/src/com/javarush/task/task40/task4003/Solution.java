3331fa15f1fd7526aa1cbdde266572769f5dcd8c
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-07 20:46:46 +0300

    JavaCollections, level10, lesson06, task4003 - done

--- 4.JavaCollections/src/com/javarush/task/task40/task4003/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4003/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task40.task4003;
 
 import javax.activation.DataHandler;
+import javax.activation.DataSource;
 import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -15,19 +16,19 @@ import java.util.Properties;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
+    public static void main(final String[] args) {
+        final Solution solution = new Solution();
         solution.sendMail("name.lastname@gmail.com", "password", "friend@gmail.com");
     }
 
     public void sendMail(final String username, final String password, final String recipients) {
-        Properties props = new Properties();
+        final Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
 
-        Session session = Session.getInstance(props,
+        final Session session = Session.getInstance(props,
                 new Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
                         return new PasswordAuthentication(username, password);
@@ -35,7 +36,7 @@ public class Solution {
                 });
 
         try {
-            Message message = new MimeMessage(session);
+            final Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(username));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
 
@@ -45,16 +46,24 @@ public class Solution {
             Transport.send(message);
             System.out.println("Письмо было отправлено.");
 
-        } catch (MessagingException e) {
+        } catch (final MessagingException e) {
             System.out.println("Ошибка при отправке: " + e.toString());
         }
     }
 
-    public static void setSubject(Message message, String subject) throws MessagingException {
+    public static void setSubject(final Message message, final String subject) throws MessagingException {
         message.setSubject(subject);
     }
 
-    public static void setAttachment(Message message, String filename) throws MessagingException {
-        message.setText(filename);
+    public static void setAttachment(final Message message, final String filename) throws MessagingException {
+        final BodyPart bodyPart = new MimeBodyPart();
+        final DataSource dataSource = new FileDataSource(filename);
+        bodyPart.setDataHandler(new DataHandler(dataSource));
+        bodyPart.setFileName(filename);
+
+        final Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(bodyPart);
+
+        message.setContent(multipart);
     }
 }
