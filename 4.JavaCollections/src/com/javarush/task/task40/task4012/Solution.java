acfb1b50f96a7e5897d98f8c3a00b496770bfc19
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-10 23:09:46 +0300

    JavaCollections, level10, lesson10, task4012 - done

--- 4.JavaCollections/src/com/javarush/task/task40/task4012/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4012/Solution.java
@@ -11,22 +11,34 @@ import java.time.temporal.ChronoUnit;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        System.out.println(isLeap(LocalDate.of(2016, 1, 1)));
+        System.out.println(isLeap(LocalDate.of(2000, 1, 1)));
 
+        System.out.println(isBefore(LocalDateTime.of(LocalDate.of(2017, 5, 11), LocalTime.now())));
+        System.out.println(isBefore(LocalDateTime.of(LocalDate.of(2017, 1, 11), LocalTime.now())));
+
+        System.out.println(addTime(LocalTime.of(10, 0, 0), 30, ChronoUnit.MINUTES));
+
+        final LocalDate firstDate = LocalDate.of(2017, 5, 10);
+        final LocalDate secondDate = LocalDate.of(2017, 5, 12);
+        System.out.println(getPeriodBetween(firstDate, secondDate).getDays());
+        System.out.println(getPeriodBetween(secondDate, firstDate));
     }
 
-    public static boolean isLeap(LocalDate date) {
-
+    public static boolean isLeap(final LocalDate date) {
+        return date.isLeapYear();
     }
 
-    public static boolean isBefore(LocalDateTime dateTime) {
-
+    public static boolean isBefore(final LocalDateTime dateTime) {
+        return dateTime.isBefore(LocalDateTime.now());
     }
 
-    public static LocalTime addTime(LocalTime time, int n, ChronoUnit chronoUnit) {
+    public static LocalTime addTime(final LocalTime time, final int n, final ChronoUnit chronoUnit) {
+        return time.plus(n, chronoUnit);
     }
 
-    public static Period getPeriodBetween(LocalDate firstDate, LocalDate secondDate) {
-
+    public static Period getPeriodBetween(final LocalDate firstDate, final LocalDate secondDate) {
+        return firstDate.isBefore(secondDate) ? Period.between(firstDate, secondDate) : Period.between(secondDate, firstDate);
     }
 }
