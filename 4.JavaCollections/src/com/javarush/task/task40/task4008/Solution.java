e8f02d6dc7b8d9642226acbd0f521e3c948d08d7
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-10 21:10:22 +0300

    JavaCollections, level10, lesson10, task4008 - done

--- 4.JavaCollections/src/com/javarush/task/task40/task4008/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4008/Solution.java
@@ -3,13 +3,14 @@ package com.javarush.task.task40.task4008;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
-/* 
+/*
 Работа с Java 8 DateTime API
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         printDate("21.4.2014 15:56:45");
         System.out.println();
         printDate("21.4.2014");
@@ -17,7 +18,36 @@ public class Solution {
         printDate("17:33:40");
     }
 
-    public static void printDate(String date) {
-        //напишите тут ваш код
+    public static void printDate(final String date) {
+        final String[] formats = {"d.M.u", "H:m:s"};
+
+        final String time = date.split(" ").length == 1 ? date : date.split(" ")[1];
+
+        for (final String format : formats) {
+            try {
+                final LocalDate localDate = LocalDate.parse(date.split(" ")[0], DateTimeFormatter.ofPattern(format));
+
+                System.out.println("День: " + localDate.getDayOfMonth());
+                System.out.println("День недели: " + localDate.getDayOfWeek().getValue());
+                System.out.println("День месяца: " + localDate.getDayOfMonth());
+                System.out.println("День года: " + localDate.getDayOfYear());
+                System.out.println("Неделя месяца: " + localDate.format(DateTimeFormatter.ofPattern("W")));
+                System.out.println("Неделя года: " + localDate.format(DateTimeFormatter.ofPattern("w")));
+                System.out.println("Месяц: " + localDate.getMonthValue());
+                System.out.println("Год: " + localDate.getYear());
+            } catch (final DateTimeParseException ignored) {
+            }
+
+            try {
+                final LocalTime localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern(format));
+
+                System.out.println("AM или PM: " + localTime.format(DateTimeFormatter.ofPattern("a")));
+                System.out.println("Часы: " + localTime.format(DateTimeFormatter.ofPattern("h")));
+                System.out.println("Часы дня: " + localTime.getHour());
+                System.out.println("Минуты: " + localTime.getMinute());
+                System.out.println("Секунды: " + localTime.getSecond());
+            } catch (final DateTimeParseException ignored) {
+            }
+        }
     }
 }
