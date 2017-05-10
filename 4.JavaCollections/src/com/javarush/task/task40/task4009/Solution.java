47b029666b7411e3af6e3569f99017845ffe87e3
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-10 22:30:37 +0300

    JavaCollections, level10, lesson10, task4009 - done

--- 4.JavaCollections/src/com/javarush/task/task40/task4009/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4009/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task40.task4009;
 import java.time.LocalDate;
 import java.time.Year;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.Locale;
 
 /* 
@@ -10,11 +11,17 @@ Buon Compleanno!
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println(weekDayOfBirthday("30.05.1984", "2015"));
+        System.out.println(weekDayOfBirthday("1.12.2015", "2016"));
     }
 
-    public static String weekDayOfBirthday(String birthday, String year) {
-        //напишите тут ваш код
+    public static String weekDayOfBirthday(final String birthday, final String year) {
+        final Year eventYear = Year.parse(year);
+        final LocalDate birthDate = LocalDate
+                .parse(birthday, DateTimeFormatter.ofPattern("d.M.u"))
+                .withYear(eventYear.getValue());
+
+        return birthDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ITALY);
     }
 }
