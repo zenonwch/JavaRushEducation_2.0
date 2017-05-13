e3213149947c67f649d9657f38f13e23194118e5
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-05-13 18:40:32 +0300

    JavaCollections, level10, lesson15, task3410 - subtask11 done

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -2,12 +2,34 @@ package com.javarush.task.task34.task3410.model;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
 
+import java.nio.file.Paths;
+
 public class Model {
     private EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("../res/levels.txt"));
 
     public static int FIELD_CELL_SIZE = 20;
 
     public void setEventListener(final EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(final int level) {
+        gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel++;
+        restart();
     }
 }
